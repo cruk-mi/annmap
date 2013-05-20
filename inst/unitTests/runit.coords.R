f7764844bc29bf9e42b1bea109a42a156a069184
Tim Yates <tyates@picr.man.ac.uk>
2013-05-20 16:24:32 +0100

    Unit test for ANNMAP-112 bug with transcript/protein CoordsToGenome

--- inst/unitTests/runit.coords.R
+++ inst/unitTests/runit.coords.R
@@ -25,3 +25,14 @@ test.huisun.ANNMAP48 = function() {
     checkTrue( length( proteinCoordsToGenome(protein.ids="ENSP00000369050", position=1, as.vector=TRUE, check.bounds=TRUE, truncate=TRUE) ) == 1, "Should return a single position" )
   }
 }
+
+test.ANNMAP112 = function() {
+  data = tryCatch( annmapConnect( "hs-test" ), error=function(e) { FALSE } )
+  if( length( data ) == 1 ) {
+    print( "Cannot find datasource 'hs-test', so skipping these tests." )
+  }
+  else {
+    checkTrue( length( transcriptCoordsToGenome( transcriptDetails( 'ENST00000280979' ) ) ) == 1, "Transcript coords failing to convert to GRanges" )
+    checkTrue( length( proteinCoordsToGenome( protein.ids="ENSP00000369050", position=1, as.vector=FALSE, check.bounds=TRUE, truncate=TRUE) ) == 1, "Should return a single GRanges position" )
+  }
+}
