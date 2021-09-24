c8d98c9851915979e0b1e313b942be1cf7222713
christopherwirth1 <wirthchris@aol.com>
2021-09-24 12:37:06 +0000

    Stop ws.R from attempting to connect to discontinued annmap webserver

--- R/ws.R
+++ R/ws.R
@@ -7,38 +7,42 @@
   if( is.null( wshost ) ) {
     wshost = 'http://annmap.cruk.manchester.ac.uk'
   }
-  data = suppressWarnings( fromJSON( file=paste( wshost, '/data/init.js', sep='' ) ) )
-  a = do.call( 'rbind', unlist( lapply( names( data$items ), function( s ) {
-    lapply( data$items[[ s ]]$versions, function( v ) {
-      list( name=paste( s, v, sep='.' ), dbname=s, display=data$items[[ s ]]$display, version=v )
-    } )
-  } ), recursive=F ) )
-  if( missing( name ) ) {
-    r = menu( apply( a, 1, function( row ) {
-      paste( row$name, ' -- ', row$display, ' v', row$version, ' (', wshost, '/?s=', row$dbname, ')', sep='' )
-    } ), title="Select a database to connect to:" )
-    if( r == 0 ) {
-      return( invisible() )
+  if ( wshost == 'http://annmap.cruk.manchester.ac.uk' ) {
+    stop( 'http://annmap.cruk.manchester.ac.uk is no longer available' )
+  } else {
+    data = suppressWarnings( fromJSON( file=paste( wshost, '/data/init.js', sep='' ) ) )
+    a = do.call( 'rbind', unlist( lapply( names( data$items ), function( s ) {
+      lapply( data$items[[ s ]]$versions, function( v ) {
+        list( name=paste( s, v, sep='.' ), dbname=s, display=data$items[[ s ]]$display, version=v )
+      } )
+    } ), recursive=F ) )
+    if( missing( name ) ) {
+      r = menu( apply( a, 1, function( row ) {
+        paste( row$name, ' -- ', row$display, ' v', row$version, ' (', wshost, '/?s=', row$dbname, ')', sep='' )
+      } ), title="Select a database to connect to:" )
+      if( r == 0 ) {
+        return( invisible() )
+      }
+      else {
+        name = a[ r, 'name' ]
+      }
     }
-    else {
-      name = a[ r, 'name' ]
+    dbs = a[ ( as.character( a[, 'name'] ) == name ), , drop=FALSE ]
+
+    if( dim( dbs )[1] == 0 ) {
+      stop( paste( "Invalid db name:", name ) )
     }
+    annmapDisconnect()
+    species = as.character( dbs[,"dbname"] )
+    version = as.character( dbs[,"version"] )
+    annmapSetParam( species=species, version=version, host=wshost, db.name=name, connected=TRUE )
+
+    cat( paste( 'Connected to ', name, ' (', wshost, ')\n', sep='' ) )
+
+    arrayType( NULL, pick.default=TRUE )
+
+    invisible( list( host=wshost, species=species, version=version ) )
   }
-  dbs = a[ ( as.character( a[, 'name'] ) == name ), , drop=FALSE ]
-
-  if( dim( dbs )[1] == 0 ) {
-    stop( paste( "Invalid db name:", name ) )
-  }
-  annmapDisconnect()
-  species = as.character( dbs[,"dbname"] )
-  version = as.character( dbs[,"version"] )
-  annmapSetParam( species=species, version=version, host=wshost, db.name=name, connected=TRUE )
-
-  cat( paste( 'Connected to ', name, ' (', wshost, ')\n', sep='' ) )
-
-  arrayType( NULL, pick.default=TRUE )
-
-  invisible( list( host=wshost, species=species, version=version ) )
 }
 
 .pretty.size = function( x ) {
@@ -54,63 +58,67 @@
   if( is.null( wshost ) ) {
     wshost = 'http://annmap.cruk.manchester.ac.uk'
   }
-  url = paste( wshost, '/data/annmapws/', paste( elements, collapse='/' ), '.js', sep='' )
+  if ( wshost == 'http://annmap.cruk.manchester.ac.uk' ) {
+    stop( 'http://annmap.cruk.manchester.ac.uk is no longer available' )
+  } else {
+    url = paste( wshost, '/data/annmapws/', paste( elements, collapse='/' ), '.js', sep='' )
 
-  .xmap.internals$debugFn( paste( 'calling', url ) )
+    .xmap.internals$debugFn( paste( 'calling', url ) )
 
-  if( annmapGetParam( 'quiet.webservice' ) == FALSE ) {
-    cat( "Fetching data from webservice...\n" )
-  }
-  json = suppressWarnings( readLines( url ) )
-  if( annmapGetParam( 'quiet.webservice' ) == FALSE ) {
-    cat( paste( "Retrieved data of size ", .pretty.size( nchar( json ) ), ". Parsing...\n", sep='' ) )
-  }
-  data = fromJSON( json )
-
-  if( !is.null( data$error ) ) {
-    stop( data$error )
-  }
-
-  if( annmapGetParam( 'quiet.webservice' ) == FALSE ) {
-    cat( paste( "Generating data.frame\n" ) )
-  }
-  if( length( data$items ) == 0 ) {
-    return( NULL )
-  }
-  else if( is.null( names( data$items ) ) ) { # list from .all query
-    parse = function( idx ) {
-      ret = data$items[[ idx ]]
-      ret = ret[ !( names( ret ) %in% c( '__type', '__id', 'synonyms' ) ) ]
-      ret[ sapply( ret, is.null ) ] = NA
-      unlist( ret )
+    if( annmapGetParam( 'quiet.webservice' ) == FALSE ) {
+      cat( "Fetching data from webservice...\n" )
     }
-    rslt = data.frame(
-      do.call( 'rbind',
-        lapply( seq_along( data$items ), parse )
-      ),
-      stringsAsFactors=F )
-  }
-  else { # Map from other query
-    innerMapping = function( idx, IN1 ) {
-      ret = data$items[[ IN1 ]][[ idx ]]
-      ret$IN1 = IN1
-      ret = ret[ !( names( ret ) %in% c( '__type', '__id', 'synonyms' ) ) ]
-      ret[ sapply( ret, is.null ) ] = NA
-      unlist( ret )
+    json = suppressWarnings( readLines( url ) )
+    if( annmapGetParam( 'quiet.webservice' ) == FALSE ) {
+      cat( paste( "Retrieved data of size ", .pretty.size( nchar( json ) ), ". Parsing...\n", sep='' ) )
     }
-    outerMapping = function( IN1 ) {
-      do.call( 'rbind', lapply( seq_along( data$items[[ IN1 ]] ), innerMapping, IN1=IN1 ) )
+    data = fromJSON( json )
+
+    if( !is.null( data$error ) ) {
+      stop( data$error )
     }
-    rslt = data.frame(
-      do.call( 'rbind', lapply( names( data$items ), outerMapping ) ),
-      stringsAsFactors=F )
+
+    if( annmapGetParam( 'quiet.webservice' ) == FALSE ) {
+      cat( paste( "Generating data.frame\n" ) )
+    }
+    if( length( data$items ) == 0 ) {
+      return( NULL )
+    }
+    else if( is.null( names( data$items ) ) ) { # list from .all query
+      parse = function( idx ) {
+        ret = data$items[[ idx ]]
+        ret = ret[ !( names( ret ) %in% c( '__type', '__id', 'synonyms' ) ) ]
+        ret[ sapply( ret, is.null ) ] = NA
+        unlist( ret )
+      }
+      rslt = data.frame(
+        do.call( 'rbind',
+          lapply( seq_along( data$items ), parse )
+        ),
+        stringsAsFactors=F )
+    }
+    else { # Map from other query
+      innerMapping = function( idx, IN1 ) {
+        ret = data$items[[ IN1 ]][[ idx ]]
+        ret$IN1 = IN1
+        ret = ret[ !( names( ret ) %in% c( '__type', '__id', 'synonyms' ) ) ]
+        ret[ sapply( ret, is.null ) ] = NA
+        unlist( ret )
+      }
+      outerMapping = function( IN1 ) {
+        do.call( 'rbind', lapply( seq_along( data$items[[ IN1 ]] ), innerMapping, IN1=IN1 ) )
+      }
+      rslt = data.frame(
+        do.call( 'rbind', lapply( names( data$items ), outerMapping ) ),
+        stringsAsFactors=F )
+    }
+    # type convert values which need it (start, end, strand)
+    if( !is.null( rslt$start ) )  rslt$start   = as.integer( rslt$start )
+    if( !is.null( rslt$end ) )    rslt$end     = as.integer( rslt$end )
+    if( !is.null( rslt$strand ) ) rslt$strand  = as.integer( rslt$strand )
+    if( !is.null( rslt$length ) ) rslt$length  = as.integer( rslt$length )
+    rslt
   }
-  # type convert values which need it (start, end, strand)
-  if( !is.null( rslt$start ) )  rslt$start   = as.integer( rslt$start )
-  if( !is.null( rslt$end ) )    rslt$end     = as.integer( rslt$end )
-  if( !is.null( rslt$strand ) ) rslt$strand  = as.integer( rslt$strand )
-  if( !is.null( rslt$length ) ) rslt$length  = as.integer( rslt$length )
-  rslt
 }
 
 .xmcws.disconnect = function() {
